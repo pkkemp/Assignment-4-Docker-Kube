2f8943c5f589543437868e693ffaeb9505c65094
Preston Kemp <preston@prestonkemp.com>
2019-10-27 21:11:52 -0500

    Assignment 2

---
+++ test.py
@@ -0,0 +1,35 @@
+from app import app
+import unittest 
+
+class FlaskBookshelfTests(unittest.TestCase): 
+
+    @classmethod
+    def setUpClass(cls):
+        pass 
+
+    @classmethod
+    def tearDownClass(cls):
+        pass 
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True 
+
+    def tearDown(self):
+        pass 
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/') 
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200) 
+
+    def test_spell_check(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        response = self.app.get('/spell_check', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
