6d334c172a6162a21ecb386b5b1765a615bc5f35
Preston Kemp <preston@prestonkemp.com>
2019-10-27 21:45:06 -0500

    updating tests

--- test.py
+++ test.py
@@ -21,24 +21,25 @@ class FlaskBookshelfTests(unittest.TestCase):
         pass 
 
     def test_home_status_code(self):
-        # sends HTTP GET request to the application
-        # on the specified path
         result = self.app.get('/') 
 
         # assert the status code of the response
         self.assertEqual(result.status_code, 200) 
 
     def test_spell_check(self):
-        # sends HTTP GET request to the application
-        # on the specified path
         response = self.app.get('/spell_check', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
     def test_login(self):
-        # sends HTTP GET request to the application
-        # on the specified path
         response = self.app.get('/login', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
         response = self.app.post('/login', data=dict(uname='pw', pword='pw', twofact='123'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_register(self):
+        response = self.app.get('/register', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.app.post('/register', data=dict(uname='pw', pword='pw', twofact='123'))
         self.assertEqual(response.status_code, 200)
